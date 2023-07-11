91a602ddec8dc3e3781c6d67797ee4b39cd962af
Joel Plotnik <joelplotnik@gmail.com>
2023-07-11 11:56:27 -0700

    Sort homepage pieces according to score and time decay

--- app/controllers/api/v1/home_controller.rb
+++ app/controllers/api/v1/home_controller.rb
@@ -1,10 +1,16 @@
 class Api::V1::HomeController < ApplicationController
     def index
-        pieces = Piece.paginate(page: params[:page], per_page: 5).order(created_at: :desc)
-        render json: pieces, include: { 
-            user: { only: [:id, :username] },
-            channel: { only: [:id, :name] },
-            votes: { only: [:user_id, :vote_type] }
-        }
-      end
-end
+      pieces = Piece.select('pieces.*, COALESCE(SUM(votes.vote_type), 0) AS score')
+                    .left_joins(:votes)
+                    .group('pieces.id')
+                    .order(Arel.sql("(score / POWER((strftime('%s', 'now') - strftime('%s', pieces.created_at)) / 3600.0 + 2, 1.8)) DESC"))
+                    .paginate(page: params[:page], per_page: 5)
+  
+      render json: pieces, include: {
+        user: { only: [:id, :username] },
+        channel: { only: [:id, :name] },
+        votes: { only: [:user_id, :vote_type] }
+      }
+    end
+  end
+  
