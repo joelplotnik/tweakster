9a3089bceb0dc8f4f860076a48899d6463b8e746
Joel Plotnik <joelplotnik@gmail.com>
2023-07-11 14:04:57 -0700

    Update piece total comments when comment is created or destroyed

--- app/controllers/api/v1/home_controller.rb
+++ app/controllers/api/v1/home_controller.rb
@@ -3,10 +3,10 @@ class Api::V1::HomeController < ApplicationController
       pieces = Piece.select('pieces.*, COALESCE(SUM(votes.vote_type), 0) AS score')
                     .left_joins(:votes)
                     .group('pieces.id')
-                    .order(Arel.sql("(score / POWER((strftime('%s', 'now') - strftime('%s', pieces.created_at)) / 3600.0 + 2, 1.8)) DESC"))
+                    .order('score DESC')
                     .paginate(page: params[:page], per_page: 5)
   
-      render json: pieces, include: {
+      render json: pieces, include: { 
         user: { only: [:id, :username] },
         channel: { only: [:id, :name] },
         votes: { only: [:user_id, :vote_type] }
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -9,5 +9,18 @@ class Comment < ApplicationRecord
     validates :user_id, presence: true
     validates :piece_id, presence: true
     validates :message, presence: true, length: { minimum: 5, maximum: 100 }
+
+    after_create :increment_total_comments
+    after_destroy :decrement_total_comments
+
+    private
+
+    def increment_total_comments
+      piece.increment!(:total_comments)
+    end
+
+    def decrement_total_comments
+      piece.decrement!(:total_comments)
+    end
   end
   
