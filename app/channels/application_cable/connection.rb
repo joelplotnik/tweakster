ee4a6c5aa7f42476d81ef5b9238e7e0c408a0e05
Joel Plotnik <joelplotnik@gmail.com>
2024-05-16 13:23:17 -0700

    Add WebSocket connection authentication via JWT in ApplicationCable

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,32 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+    end
+
+    protected
+
+    def find_verified_user
+      token = request.params[:token].split(' ').last
+
+      if token
+        begin
+          # Decode the token using the secret key
+          decoded_token = JWT.decode(token, Rails.application.secret_key_base).first
+          user_id = decoded_token['sub']
+
+          # Find the user based on the decoded token
+          User.find(user_id)
+        rescue JWT::DecodeError => e
+          # Handle the case where token decoding fails
+          logger.error "JWT decode error: #{e.message}"
+          reject_unauthorized_connection
+        end
+      else
+        reject_unauthorized_connection
+      end
+    end
   end
 end
--- app/channels/messages_channel.rb
+++ app/channels/messages_channel.rb
@@ -1,5 +1,9 @@
 class MessagesChannel < ApplicationCable::Channel
   def subscribed
+    user = current_user
+    
+    puts "CURRENT USER: ", user.username
+
     stream_from "MessagesChannel"
   end
 
