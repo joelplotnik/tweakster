34267f976a58440ecbec2136f4517fa53deb866e
Joel Plotnik <joelplotnik@gmail.com>
2024-06-18 11:55:01 -0700

    Configure Action Mailer and Action Cable to use port 5100

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -65,8 +65,8 @@ Rails.application.configure do
   # config.action_view.annotate_rendered_view_with_filenames = true
 
   # Uncomment if you wish to allow Action Cable access from any origin.
-  config.action_cable.url = 'ws://localhost:3000/cable'
+  config.action_cable.url = 'ws://localhost:5100/cable'
   config.action_cable.disable_request_forgery_protection = true
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 5100 }
 end
