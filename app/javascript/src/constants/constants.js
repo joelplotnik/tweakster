2f5c9f314fb04990444e376cf16f5f6e03f37eb6
Tanner Mares <tannermares@gmail.com>
2024-07-12 21:07:48 -0700

    force ssl and use new real domain

--- app/javascript/src/constants/constants.js
+++ app/javascript/src/constants/constants.js
@@ -1,9 +1,9 @@
 export const API_URL =
   Tweakster.Config.env === 'development'
     ? 'http://localhost:5100/api/v1'
-    : 'http://tweakster.us-west-1.elasticbeanstalk.com/api/v1'
+    : 'https://tweakster.com/api/v1'
 export const WS_URL =
   Tweakster.Config.env === 'development'
     ? 'ws://localhost:5100/cable'
-    : 'ws://tweakster.us-west-1.elasticbeanstalk.com/cable'
+    : 'ws://tweakster.com/cable'
 export const EXPIRED_TOKEN = 'EXPIRED'
